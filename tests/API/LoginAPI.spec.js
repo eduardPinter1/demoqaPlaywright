f0251de47bdb9d18d9ef49bfa1c28ec8d558c5b5
Eduard Pinter <eduard.pinter@automaticity.rs>
2024-01-16 11:45:21 +0100

    refactor of tests for better usage and flexibility

--- tests/API/LoginAPI.spec.js
+++ tests/API/LoginAPI.spec.js
@@ -1,75 +1,46 @@
-const { test, request, expect } = require('@playwright/test');
-const urlApi = JSON.parse(JSON.stringify(require('../../fixtures/ApiUrl.json')));
+const { test, request } = require('@playwright/test');
+const { BAD_REQUEST, NOT_ACCEPTABLE, OK } = require('../../utils/statusCodes');
 const userData = JSON.parse(JSON.stringify(require('../../fixtures/userData.json')));
-const {UtilsFunctions} = require('../../utils/UtilsFunctions');
-const utilsFunctions = new UtilsFunctions();
+const { ApiUtils } = require('../../utils/ApiUtils');
 
-const credentialsPositive = {
+let apiUtil;
+let apiContext;
+
+let credentials = {
    "password": userData.login.password,
    "userName": userData.login.username
 }
-
-const credentialsNegative = {
+const credEmptyUsername = {
    "password": userData.login.password,
-   "userName": `${userData.login.username}e`
+   "userName": ""
 }
 
 
-let apiContext;
-
 test.beforeAll(async () => {
    apiContext = await request.newContext();
+   apiUtil = new ApiUtils(apiContext);
 })
 
 test("Successful login", async () => {
-   const registrationResponse = await apiContext.post(urlApi.loginApi,
-      {
-         data: credentialsPositive
-
-      })
-   expect(registrationResponse.status()).toBe(200);
+   await apiUtil.validLogin(credentials, OK);
 
 })
 
 test("Unsuccessful login", async () => {
-   const registrationResponse = await apiContext.post(urlApi.loginApi,
-      {
-         data: credentialsNegative
-
-      })
-   expect(registrationResponse.status()).toBe(400);
-   const responseBody = await utilsFunctions.parseResText(registrationResponse);
-   expect(responseBody.result).toBe(userData.login.resultMessage)
+   credentials.userName = `${userData.login.username}e`
+   await apiUtil.nonValidLogin(credentials, NOT_ACCEPTABLE, false, userData.login.resultMessage);
 
 })
 
 test("Unsuccessful login - password empty", async () => {
-   const registrationResponse = await apiContext.post(urlApi.loginApi,
-      {
-         data: {
-            "password": "",
-            "userName": userData.login.username,
-          }
-
-      })
-   expect(registrationResponse.status()).toBe(400);
-   const responseBody = await utilsFunctions.parseResText(registrationResponse);
-   expect(responseBody.message).toBe(userData.registration.missingCredentialsMessage);
+   credentials.password = "";
+   await apiUtil.nonValidLogin(credentials, BAD_REQUEST, true, userData.registration.missingCredentialsMessage)
 
 })
 
 test("Unsuccessful login - username empty", async () => {
-   const registrationResponse = await apiContext.post(urlApi.loginApi,
-      {
-         data: {
-            "password": userData.login.password,
-            "userName": "",
-          }
-
-      })
-   expect(registrationResponse.status()).toBe(400);
-   const responseBody = await utilsFunctions.parseResText(registrationResponse);
-   expect(responseBody.message).toBe(userData.registration.missingCredentialsMessage);
+   credentials.userName = "";
+   await apiUtil.nonValidLogin(credentials, BAD_REQUEST, true, userData.registration.missingCredentialsMessage)
 
 })
 
--- tests/API/RegisterApi.spec.js
+++ tests/API/RegisterApi.spec.js
@@ -1,91 +1,52 @@
 const {test,request, expect} = require('@playwright/test');
+const { CREATED, BAD_REQUEST, NOT_ACCEPTABLE } = require('../../utils/statusCodes');
 const urlApi = JSON.parse(JSON.stringify(require('../../fixtures/ApiUrl.json')));
 const userData = JSON.parse(JSON.stringify(require('../../fixtures/userData.json')));
 const {UtilsFunctions} = require('../../utils/UtilsFunctions');
 const utilsFunctions = new UtilsFunctions();
+const { ApiUtils } = require('../../utils/ApiUtils');
 
+let apiUtil;
 let apiContext;
+
+let userCred = {
+    "password" : userData.registration.passwordPass,
+    "userName" : userData.login.username,
+}
 
 test.beforeAll(async() => {
     apiContext = await request.newContext();
+    apiUtil = new ApiUtils(apiContext);
 })
 
 test("Registration successful", async () => {
-    
-    const username = userData.registration.username + utilsFunctions.getRandomInt(1000);
-    const registrationResponse =  await apiContext.post(urlApi.registerApi,
-    {
-        data: {
-            "password" : userData.registration.passwordPass,
-            "userName" : username,
-        }
-
-     } )
-    expect(registrationResponse.status()).toBe(201);
-    const responseBody = await utilsFunctions.parseResText(registrationResponse);
-    expect(responseBody.username).toBe(username)
+    userCred.userName = userData.registration.username + utilsFunctions.getRandomInt(1000);
+    console.log(userCred);
+    await apiUtil.validRegistration(userCred,CREATED,userCred.userName);
 
 })
 
 test("Registration failed - user already exists", async () => {
-        const registrationResponse =  await apiContext.post(urlApi.registerApi,
-    {
-        data: {
-            "password" : userData.registration.passwordPass,
-            "userName" : userData.login.username,
-        }
-
-     } )
-    expect(registrationResponse.status()).toBe(406);
-    const responseBody = await utilsFunctions.parseResText(registrationResponse);
-    expect(responseBody.message).toBe(userData.registration.usernamePresentMessage);
+    userCred.password = userData.registration.passwordPass;
+    userCred.userName = userData.registration.username;
+    apiUtil.nonValidRegistration(userCred, NOT_ACCEPTABLE, userData.registration.usernamePresentMessage);
 
 })
 
 test("Registration failed - password conditions not met", async () => {
-    
-    const registrationResponse =  await apiContext.post(urlApi.registerApi,
-    {
-        data: {
-            "password" : userData.registration.passwordWrong,
-            "userName" : userData.registration.username,
-        }
-
-     } )
-    expect(registrationResponse.status()).toBe(400);
-    const responseBody = await utilsFunctions.parseResText(registrationResponse);
-    expect(responseBody.message).toBe(userData.registration.passwordMessage);
+    userCred.password = userData.registration.passwordWrong;
+    await apiUtil.nonValidRegistration(userCred, BAD_REQUEST,userData.registration.passwordMessage)
 
 })
 
 test("Registration failed - password empty", async () => {
-    
-    const registrationResponse =  await apiContext.post(urlApi.registerApi,
-    {
-        data: {
-            "password" : "",
-            "userName" : userData.registration.username,
-        }
-
-     } )
-    expect(registrationResponse.status()).toBe(400);
-    const responseBody = await utilsFunctions.parseResText(registrationResponse);
-    expect(responseBody.message).toBe(userData.registration.missingCredentialsMessage);
+    userCred.password = "";
+    await apiUtil.nonValidRegistration(userCred, BAD_REQUEST,userData.registration.missingCredentialsMessage)
 
 })
 
 test("Registration failed - username empty", async () => {
-    
-    const registrationResponse =  await apiContext.post(urlApi.registerApi,
-    {
-        data: {
-            "password" : userData.registration.passwordPass,
-            "userName" : "",
-        }
-
-     } )
-    expect(registrationResponse.status()).toBe(400);
-    const responseBody = await utilsFunctions.parseResText(registrationResponse);
-    expect(responseBody.message).toBe(userData.registration.missingCredentialsMessage);
+    userCred.userName = "";
+    await apiUtil.nonValidRegistration(userCred, BAD_REQUEST,userData.registration.missingCredentialsMessage)
 
 })
