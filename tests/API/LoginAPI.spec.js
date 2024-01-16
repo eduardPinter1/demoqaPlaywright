f54ad87a235c4de4859ccc79e338181131bd3587
Eduard Pinter <eduard.pinter@automaticity.rs>
2024-01-16 08:21:47 +0100

    moving user credentials to separate variables for test usage

--- tests/API/LoginAPI.spec.js
+++ tests/API/LoginAPI.spec.js
@@ -1,37 +1,40 @@
-//e p ep - user exist
-const {test,request, expect} = require('@playwright/test');
+const { test, request, expect } = require('@playwright/test');
 const urlApi = JSON.parse(JSON.stringify(require('../../fixtures/ApiUrl.json')));
 const userData = JSON.parse(JSON.stringify(require('../../fixtures/userData.json')));
+
+const credentialsPositive = {
+   "password": userData.login.password,
+   "userName": userData.login.username,
+}
+
+const credentialsNegative = {
+   "password": userData.login.password,
+   "userName": `${userData.login.username}e`,
+}
 
 
 let apiContext;
 
-test.beforeAll(async() => {
+test.beforeAll(async () => {
    apiContext = await request.newContext();
 })
 
 test("Successful login", async () => {
-   const registrationResponse =  await apiContext.post(urlApi.loginApi,
-   {
-       data: {
-           "password" : userData.login.password,
-           "userName" : userData.login.username,
-       }
+   const registrationResponse = await apiContext.post(urlApi.loginApi,
+      {
+         data: { credentialsPositive }
 
-    } )
+      })
    expect(registrationResponse.status()).toBe(200);
 
 })
 
 test("Unsuccessful login", async () => {
-   const registrationResponse =  await apiContext.post(urlApi.loginApi,
-   {
-       data: {
-           "password" : userData.login.password,
-           "userName" : userData.login.username+"e",
-       }
+   const registrationResponse = await apiContext.post(urlApi.loginApi,
+      {
+         data: { credentialsNegative }
 
-    } )
+      })
    expect(registrationResponse.status()).toBe(200);
    const responseBody = JSON.parse(await registrationResponse.text());
    expect(responseBody.result).toBe(userData.login.resultMessage)
