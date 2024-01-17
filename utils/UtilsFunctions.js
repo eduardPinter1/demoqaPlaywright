bfeb2f3052f6ad9a2249054e413546d80d5e9063
Eduard Pinter <eduard.pinter@automaticity.rs>
2024-01-17 11:44:32 +0100

    Changes to Utils function and rename of API utils

--- utils/UtilsFunctions.js
+++ utils/UtilsFunctions.js
@@ -1,16 +1,16 @@
 class UtilsFunctions {
 
 
-    async parseResText(response){
-            return JSON.parse(await response.text());
+  async parseResText(response) {
+    return JSON.parse(await response.text());
 
-    }
-    
-    getRandomInt(max) {
-        return Math.floor(Math.random() * max);
+  }
 
-      }
+  getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+
+  }
 
 }
 
-module.exports = {UtilsFunctions}
+module.exports = { UtilsFunctions }
