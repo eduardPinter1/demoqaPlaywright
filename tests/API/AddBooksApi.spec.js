21a63f3a540db91c37696f5538be5852b5afef72
Eduard Pinter <eduard.pinter@automaticity.rs>
2024-01-22 08:47:16 +0100

    Add and delete books

--- tests/API/AddBooksApi.spec.js
+++ tests/API/AddBooksApi.spec.js
@@ -1,54 +1,124 @@
-const { test, request } = require('@playwright/test');
+import { test } from '../../modules/base';
 const userData = JSON.parse(JSON.stringify(require('../../fixtures/userData.json')));
-const { LoginApiUtils } = require("../../utils/LoginApiUtils")
-const { BookstoreApiUtils } = require("../../utils/BookstoreApiUtils");
-let userPayLoad = { password: userData.login.password, userName: userData.login.username };
-let apiContext;
+const { BAD_REQUEST, UNAUTHORIZED } = require('../../utils/statusCodes');
 let response;
 let token;
 let userId;
-let booksApi;
 let bookIsbn;
 
-test.describe.configure({ mode: 'serial' });
+//test.describe.configure({ mode: 'serial' });
 
-test.beforeAll(async () => {
-    apiContext = await request.newContext();
-    const api = new LoginApiUtils(apiContext)
-    response = await api.getTokenAndUserId(userPayLoad);
+test.beforeEach(async ({ loginApiUtils, addBooksApi }) => {
+    response = await loginApiUtils.getTokenAndUserId({ username: userData.login.username, password: userData.login.password });
+    loginApiUtils.authorizeUser({ username: userData.login.username, password: userData.login.password });
     token = await response.token;
     userId = await response.userId;
-    booksApi = new BookstoreApiUtils(apiContext, token);
-    bookIsbn = await booksApi.getBookIsbnByIndex(0);
+    bookIsbn = await addBooksApi.getBookIsbnByIndex(0);
 
 })
 
-test.afterAll(async () => {
-    booksApi.deleteBookByIsbn(bookIsbn, userId, token);
+test.describe("Positive cases", async () => {
+    test("Post books - happy flow", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: userId, token: token });
+
+    })
 })
 
-test("Post books - happy flow", async () => {
-    await booksApi.postBooks(bookIsbn, userId, token, false, false, false);
+test.describe("Negative cases", async () => {
+    test("Post books - incorrect book isbn", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: `${bookIsbn}4`, userId: userId, token: token, incorrectIsbn: true, statusCode: BAD_REQUEST });
 
+    })
+
+    test("Post books - wrong userId", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: `${userId}e`, token: token, wrongUserId: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - book isbn value empty", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: "", userId: userId, token: token, incorrectIsbn: true, statusCode: BAD_REQUEST })
+    })
+
+    test("Post books - userId empty", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: "", token: token, wrongUserId: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - No authentication token", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: userId, noAuth: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - isbn integer", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: 1, userId: userId, token: token, incorrectIsbn: true, statusCode: BAD_REQUEST });
+    })
+
+    test("Post books - isbn double", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: 1.1, userId: userId, token: token, incorrectIsbn: true, statusCode: BAD_REQUEST });
+    })
+
+    test("Post books - isbn boolean", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: true, userId: userId, token: token, incorrectIsbn: true, statusCode: BAD_REQUEST });
+    })
+
+    test("Post books - isbn array", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: ["bookIsbn"], userId: userId, token: token, incorrectIsbn: true, statusCode: BAD_REQUEST });
+    })
+
+    test("Post books - isbn object", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: { value: "1" }, userId: userId, token: token, incorrectIsbn: true, statusCode: BAD_REQUEST });
+    })
+
+    test("Post books - isbn null", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: null, userId: userId, token: token, incorrectIsbn: true, statusCode: BAD_REQUEST });
+    })
+
+    test("Post books - userId integer", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: 1, token: token, wrongUserId: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - userId double", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: 1.1, token: token, wrongUserId: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - userId boolean", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: true, token: token, wrongUserId: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - userId array", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: ["userId"], token: token, wrongUserId: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - userId object", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: { value: "1" }, token: token, wrongUserId: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - userId null", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: null, token: token, wrongUserId: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - token integer", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: userId, token: 1, noAuth: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - token double", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: userId, token: 1.1, noAuth: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - token boolean", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: userId, token: true, noAuth: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - token array", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: userId, token: ["token"], noAuth: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - token object", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: userId, token: { value: token }, noAuth: true, statusCode: UNAUTHORIZED });
+    })
+
+    test("Post books - token null", async ({ addBooksApi }) => {
+        await addBooksApi.postBooks({ isbn: bookIsbn, userId: userId, token: null, noAuth: true, statusCode: UNAUTHORIZED });
+    })
 })
 
-test("Post books - incorrect book isbn", async () => {
-    await booksApi.postBooks(`${bookIsbn}4`, userId, token, true, false, false);
-
-})
-
-test("Post books - wrong userId", async () => {
-    await booksApi.postBooks(bookIsbn, `${userId}e`, token, false, true, false);
-})
-
-test("Post books - book isbn value empty", async () => {
-    await booksApi.postBooks("", userId, token, true, false, false)
-})
-
-test("Post books - userId empty", async () => {
-    await booksApi.postBooks(bookIsbn, "", token, false, true, false);
-})
-
-test("Post books - No authentication token", async () => {
-    await booksApi.postBooks(bookIsbn, userId, "", false, false, true);
+test.afterAll(async ({ addBooksApi }) => {
+    await addBooksApi.deleteAllBooks({ userId: userId, token: token });
 })
--- tests/API/DeleteAllBooksApi.spec.js
+++ tests/API/DeleteAllBooksApi.spec.js
@@ -1,55 +1,108 @@
-const { test, request } = require('@playwright/test');
+import { test } from '../../modules/base';
 const userData = JSON.parse(JSON.stringify(require('../../fixtures/userData.json')));
-const { LoginApiUtils } = require("../../utils/LoginApiUtils")
-const { BookstoreApiUtils } = require("../../utils/BookstoreApiUtils");
-let userPayLoad = { password: userData.login.password, userName: userData.login.username };
-let apiContext;
+const { UNAUTHORIZED } = require('../../utils/statusCodes');
 let response;
 let token;
 let userId;
-let booksApi;
 let booksIsbn = [];
 
 test.describe.configure({ mode: 'serial' });
 
-test.beforeAll(async () => {
-    apiContext = await request.newContext();
-    const api = new LoginApiUtils(apiContext)
-    response = await api.getTokenAndUserId(userPayLoad);
+test.beforeAll(async ({ loginApiUtils, addBooksApi }) => {
+    response = await loginApiUtils.getTokenAndUserId({ username: userData.login.username, password: userData.login.password });
+    loginApiUtils.authorizeUser({ username: userData.login.username, password: userData.login.password });
     token = await response.token;
     userId = await response.userId;
-    booksApi = new BookstoreApiUtils(apiContext);
-    booksIsbn.push(await booksApi.getBookIsbnByIndex(0), await booksApi.getBookIsbnByIndex(1));
+    booksIsbn.push(await addBooksApi.getBookIsbnByIndex(0), await addBooksApi.getBookIsbnByIndex(1));
     booksIsbn.forEach(element => {
-        booksApi.postBooks(element, userId, token, false, false, false)
+        addBooksApi.postBooks({ isbn: element, userId: userId, token: token });
     });
 })
 
-test.afterAll(async () => {
-    await booksApi.deleteAllBooks(userId, token, false, false);
+test.afterAll(async ({ addBooksApi }) => {
+    await addBooksApi.deleteAllBooks({ userId: userId, token: token });
 })
 
-test("Delete All Books - happy flow", async () => {
-    await booksApi.deleteAllBooks(userId, token, false, false);
+test.describe("Positive test cases", async ({ }) => {
+
+    test("Delete All Books - happy flow", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: userId, token: token });
+
+    })
+})
+
+test.describe("Negative test cases", async ({ }) => {
+
+    test("Delete All Books - wrong userId", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: `${userId}e`, token: token, idEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - no userId", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: "", token: token, idEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - userId integer", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: 123, token: token, idEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - userId boolean", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: true, token: token, idEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - userId array", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: ["userId"], token: token, idEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - userId object", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: { value: userId }, token: token, idEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - userId null", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: null, token: token, idEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - wrong token", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: userId, token: `${token}e`, tokenEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - no token", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: userId, token: "", tokenEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - token integer", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: userId, token: 123, tokenEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - token boolean", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: userId, token: true, tokenEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - token array", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: userId, token: [`${token}`], tokenEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - token object", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: userId, token: { value: token }, tokenEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
+
+    test("Delete All Books - token null", async ({ addBooksApi }) => {
+        await addBooksApi.deleteAllBooks({ userId: userId, token: null, tokenEmptyIncorrect: true, statusCode: UNAUTHORIZED });
+
+    })
 
 })
 
-test("Delete All Books - wrong userId", async () => {
-    await booksApi.deleteAllBooks(`${userId}e`, token, true, false);
 
-})
-
-test("Delete All Books - no userId", async () => {
-    await booksApi.deleteAllBooks("", token, true, false);
-
-})
-
-test("Delete All Books - wrong token", async () => {
-    await booksApi.deleteAllBooks(userId, `${token}e`, false, true);
-
-})
-
-test("Delete All Books - no token", async () => {
-    await booksApi.deleteAllBooks(userId, "", false, true);
-
-})
